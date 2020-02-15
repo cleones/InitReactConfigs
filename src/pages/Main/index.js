32dbf43bd80252960db3a20df7b3974eaf75fb74
Cleones <cleones06@gmail.com>
2020-02-15 12:43:55 -0300

    styled-component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Title } from './styles';
 
 const Main = () => {
-  return <h1>Main</h1>;
+  return (
+    <Title error>
+      Main
+      <small> texto menos</small>
+    </Title>
+  );
 };
 
 export default Main;
